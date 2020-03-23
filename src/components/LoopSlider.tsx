b511c6e8dd767e7d45839b747c9d5499b63cac70
いてづき <mistolteen0127@gmail.com>
2020-03-24 00:13:48 +0900

    styled-components導入

--- src/components/LoopSlider.tsx
+++ src/components/LoopSlider.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
 type Props = {
   imagePaths: string[]
 }
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 const LoopSlider: React.FC<Props> = ({ imagePaths }) => {
   return (
-    <div>
-      {imagePaths.map(image => <img src={image} alt={image} />)}
-    </div>
+    <>
+      <Title>Title</Title>
+      <div>
+        {imagePaths.map(image => <img src={image} alt={image} />)}
+      </div>
+    </>
+
   )
 }
 
