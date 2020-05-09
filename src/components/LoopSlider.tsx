b9303782805733b88f52447d12f2f7b281c47d7b
いてづき <mistolteen0127@gmail.com>
2020-05-10 00:02:33 +0900

    無限スクロールが動作するところまで

--- src/components/LoopSlider.tsx
+++ src/components/LoopSlider.tsx
@@ -1,31 +1,61 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const LoopWrap = styled.div`
+  display: flex;
+  overflow: hidden;
+  width: auto;
+`
+const LoopAnimation = keyframes`
+  0% {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+`
+
+const LoopWrapChildFirst = styled.div`
+  display: flex;
+  width: auto;
+  animation: ${LoopAnimation} 50s -25s linear infinite;
+`
+
+const LoopWrapImg = styled.img`
+  height: 100%;
+`
+
+const LoopAnimation2 = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-200%);
+  }
+`
+
+const LoopWrapChildSecond = styled.div`
+  display: flex;
+  width: auto;
+  animation: ${LoopAnimation2} 50s linear infinite;
+`
 
 type Props = {
   imagePaths: string[]
 }
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
-
 const LoopSlider: React.FC<Props> = ({ imagePaths }) => {
   return (
     <>
-      <Title>Title</Title>
-      <div>
-        {imagePaths.map(image => <img src={image} alt={image} />)}
-      </div>
+      <LoopWrap>
+        <LoopWrapChildFirst>
+          {imagePaths.map(image => <LoopWrapImg src={image} alt={image} key={image} />)}
+        </LoopWrapChildFirst>
+        <LoopWrapChildSecond>
+          {imagePaths.map(image => <LoopWrapImg src={image} alt={image} key={image} />)}
+        </LoopWrapChildSecond>
+      </LoopWrap>
     </>
-
   )
 }
 
